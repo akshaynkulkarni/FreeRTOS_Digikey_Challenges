98eb40a9c39672e56fe168430651676e6ed00878
Akshay Narahari Kulkarni <akshaynkulkarni@gmail.com>
2023-06-07 23:08:03 +0200

    Soln9FreeRTOS: Hardware Interrupt Challenge, Initial code
    This patch is inspired from challenge at  www.digiKey.in
    (https://www.digikey.in/en/maker/projects/introduction-to-
    rtos-solution-to-part-9-hardware-interrupts/3ae7a68462584e1eb408e1638002e9ed)
    
    Challenge:
    Your challenge is to create a sampling, processing, and interface system
    using hardware interrupts and whatever kernel objects (e.g. queues,
    mutexes, and semaphores) you might need.
    
    You should implement a hardware timer in the ESP32 that samples from
    an ADC pin once every 100 ms. This sampled data should be copied to
    a double buffer (you could also use a circular buffer).
    Whenever one of the buffers is full, the ISR should notify Task A.
    
    Task A, when it receives notification from the ISR, should wake up
    and compute the average of the previously collected 10 samples.
    Note that during this calculation time,the ISR may trigger again.
    This is where a double (or circular) buffer will help: you can process
    one buffer while the other is filling up.
    
    When Task A is finished, it should update a global floating point
    variable that contains the newly computed average. Do not assume
    that writing to this floating point variable will take a single
    instruction cycle! You will need to protect that action as we
    saw in the queue episode.
    
    Task B should echo any characters received over the serial port
    back to the same serial port. If the command “avg” is entered,
    it should display whatever is in the global average variable.
    
    This is like a “final project” in that you will need to use
    many of the concepts we covered in previous lectures and challenges.
    
    TBD: Sync Mechanism, buffer full/ empty signalling, etc

---
+++ Soln9FreeRTOS/main/Soln9FreeRTOS.cpp
@@ -0,0 +1,204 @@
+#include <atomic>
+#include <cstring>
+#include <iostream>
+#include <string>
+
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+
+#include "driver/gpio.h"
+#include "driver/uart.h"
+
+#include "esp_err.h"
+#include "esp_log.h"
+#include "freertos/semphr.h"
+#include "freertos/timers.h"
+
+
+#include "driver/adc.h"
+#include "esp_adc_cal.h"
+
+#include "esp_timer.h"
+
+#define UART_DELAY (100U / portTICK_RATE_MS)
+#define ADC_DELAY_US (10000U)
+
+#define TASK_STACK_SIZE (12U * 1024U) // 12kb
+
+TaskHandle_t adc_processing_task_handle = NULL;
+TaskHandle_t uart_task_handle = NULL;
+
+constexpr std::string_view adc_processing_task_TAG = "ADC_processing_task";
+constexpr std::string_view uart_task_TAG = "uart_task";
+constexpr std::string_view app_main_task_TAG = "app_main_write_task";
+
+constexpr uart_port_t uart_port_num = UART_NUM_0;
+constexpr size_t uart_buff_size = 512U;
+constexpr size_t uart_queue_len = 10U;
+QueueHandle_t uart_queue;
+
+std::atomic<float> avg_adc = {0.0f};
+
+static esp_timer_handle_t timer = NULL;
+static esp_adc_cal_characteristics_t adc1_chars;
+
+const uint8_t c_queue_len = 10;
+volatile uint32_t c_queue[c_queue_len];
+uint8_t head = 0;
+uint8_t tail = 0;
+
+void IRAM_ATTR ADC_Timer_ISR(void *params) {
+
+  // Producer: insert the raw adc value into the queue
+  c_queue[head] = adc1_get_raw(ADC1_CHANNEL_5);
+  head = (head + 1) % c_queue_len;
+}
+
+void ADC_post_processing_Task(void *params) {
+  uint32_t adc_sum = 0;
+  while (true) {
+
+    adc_sum = 0;
+    // consumer task
+    for (uint8_t i = 0; i < c_queue_len; i++) {
+      adc_sum += c_queue[tail];
+      tail = ((tail + 1) % c_queue_len);
+    }
+    avg_adc.store((float)esp_adc_cal_raw_to_voltage(
+        ((float)adc_sum / c_queue_len), &adc1_chars));
+    vTaskDelay(UART_DELAY);
+  }
+  vTaskDelete(NULL);
+}
+
+esp_err_t hw_timer_init() {
+  const esp_timer_create_args_t timer_args = {.callback = &ADC_Timer_ISR,
+                                              .arg = NULL,
+                                              .dispatch_method = ESP_TIMER_TASK,
+                                              .name = "ADC_Timer_ISR",
+                                              .skip_unhandled_events = false};
+
+  esp_err_t err = esp_timer_create(&timer_args, &timer);
+  if (ESP_OK != err) {
+    ESP_LOGE("hw_timer_init", "Could not create HW timer");
+    return err;
+  }
+  err = esp_timer_start_periodic(timer, ADC_DELAY_US);
+  if (ESP_OK != err) {
+    ESP_LOGE("hw_timer_init", "Could not start HW timer");
+    return err;
+  }
+  return ESP_OK;
+}
+
+esp_err_t adc_init() {
+  esp_adc_cal_value_t adc_cal_value = esp_adc_cal_characterize(
+      ADC_UNIT_1, ADC_ATTEN_DB_11,
+      static_cast<adc_bits_width_t>(ADC_WIDTH_BIT_DEFAULT), 0, &adc1_chars);
+  if (ESP_ADC_CAL_VAL_EFUSE_VREF == adc_cal_value)
+    ESP_LOGI("adc_init",
+             "Characterization based on reference voltage stored in eFuse");
+  else if (ESP_ADC_CAL_VAL_EFUSE_TP == adc_cal_value)
+    ESP_LOGI("adc_init",
+             "Characterization based on Two Point values stored in eFuse");
+  else if (ESP_ADC_CAL_VAL_DEFAULT_VREF == adc_cal_value)
+    ESP_LOGI("adc_init",
+             "Characterization based on Two Point values stored in eFuse");
+  else {
+    ESP_LOGE("adc_init", "Cannot characterize ADC");
+    return ESP_FAIL;
+  }
+  esp_err_t err =
+      adc1_config_width(static_cast<adc_bits_width_t>(ADC_WIDTH_BIT_DEFAULT));
+  if (err != ESP_OK) {
+    ESP_LOGE("adc_init", "Error Config width");
+  }
+  err = (adc1_config_channel_atten(ADC1_CHANNEL_5, ADC_ATTEN_DB_11));
+
+  if (err != ESP_OK) {
+    ESP_LOGI("adc_init", "Error in config_channel_atten: %#x", err);
+    return err;
+  }
+
+  return hw_timer_init();
+}
+
+esp_err_t uart_init() {
+
+  const uart_config_t uart_config = {
+      .baud_rate = 115200,
+      .data_bits = UART_DATA_8_BITS,
+      .parity = UART_PARITY_DISABLE,
+      .stop_bits = UART_STOP_BITS_1,
+      .flow_ctrl = UART_HW_FLOWCTRL_DISABLE,
+      //.rx_flow_ctrl_thresh = 0
+  };
+
+  esp_err_t err = uart_param_config(uart_port_num, &uart_config);
+  err |= uart_set_pin(UART_NUM_0, 1, 3, -1, -1);
+
+  err |= uart_driver_install(uart_port_num, uart_buff_size, uart_buff_size,
+                             uart_queue_len, &uart_queue, 0);
+  return err;
+}
+
+void uart_task(void *params) {
+
+  while (true) {
+    int ret = 0;
+    static char read_buff[100] = {0};
+    memset(read_buff, 0, strlen(read_buff)); // clear the local buffer
+    do {
+      ret = uart_read_bytes(uart_port_num, (void *)read_buff,
+                            (sizeof(read_buff) - 1), 10 * UART_DELAY);
+    } while (ret == 0);
+
+    if (ret == -1) {
+      ESP_LOGE(uart_task_TAG.data(), "UART Read Error");
+      continue;
+    }
+
+    if (ret) {
+      uart_write_bytes(uart_port_num, (const char *)"\r", strlen("\r"));
+      uart_write_bytes(uart_port_num, (const char *)read_buff,
+                       strlen(read_buff));
+      uart_write_bytes(uart_port_num, (const char *)"\n\r", strlen("\n\r"));
+
+      if (!strcmp("avg\r", read_buff) || !strcmp("avg\n", read_buff)) {
+        ESP_LOGI(uart_task_TAG.data(), "Average is %f", avg_adc.load());
+      }
+    }
+    vTaskDelay(UART_DELAY);
+  }
+  vTaskDelete(NULL);
+}
+
+extern "C" void app_main(void) {
+
+  ESP_ERROR_CHECK(adc_init());
+  ESP_ERROR_CHECK(uart_init());
+
+  BaseType_t xReturn0 =
+      xTaskCreate(&ADC_post_processing_Task, adc_processing_task_TAG.data(),
+                  TASK_STACK_SIZE, NULL, 1, &adc_processing_task_handle);
+
+  if (xReturn0 != pdPASS) {
+    ESP_LOGE(app_main_task_TAG.data(), "Failed to create task: %s",
+             adc_processing_task_TAG.data());
+    adc_processing_task_handle = NULL;
+    vTaskDelete(NULL);
+  }
+
+  BaseType_t xReturn1 =
+      xTaskCreate(&uart_task, uart_task_TAG.data(), TASK_STACK_SIZE, NULL, 1,
+                  &uart_task_handle);
+
+  if (xReturn1 != pdPASS) {
+    ESP_LOGE(app_main_task_TAG.data(), "Failed to create task: %s",
+             uart_task_TAG.data());
+    uart_task_handle = NULL;
+    vTaskDelete(NULL);
+  }
+
+  vTaskDelete(NULL);
+}
